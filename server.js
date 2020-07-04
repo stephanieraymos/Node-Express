1ad41b49fa817d9b3a06b2118c7aced30c2e77e2
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-03 19:03:37 -0700

    Updated server to use Morgan

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 //Since express was installed into node modules; we don't have to give it a filepath. It will automatically look in node-modules
 
+const morgan = require('morgan');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 //returns an express server application. This will now be available under this variable name (app)
+app.use(morgan('dev'));
+
+//__dirname refers to absolute path of current directory of the file that it's in
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
