0c693a67fbb44f0c8a8d69f663a044138a2f4e77
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-03 20:14:04 -0700

    Simple REST

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 //Since express was installed into node modules; we don't have to give it a filepath. It will automatically look in node-modules
+const bodyParser = require('body-parser');
 
 const morgan = require('morgan');
 
@@ -9,6 +10,49 @@ const port = 3000;
 const app = express();
 //returns an express server application. This will now be available under this variable name (app)
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/campsites', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+});
+
+app.get('/campsites', (req, res) => {
+  res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+  res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+  res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+  res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+  res.statusCode = 403;
+  res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+  res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+  res.end(`Will update the campsite: ${req.body.name}
+      with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+  res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 
 //__dirname refers to absolute path of current directory of the file that it's in
 app.use(express.static(__dirname + '/public'));
